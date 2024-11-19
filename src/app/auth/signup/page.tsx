0fa5792a0378c6af1c92f1645acf179ea7721631
Dahyun Kwon <142938702+dhk486@users.noreply.github.com>
2024-11-18 21:24:26 -1000

    src\app\signup Update("Creating User")
    Add FirstName, Last Name, User Name, Musical Goals, Instrument, Experience Level, Select Musical Tastes boxes in page.tsx in signup folder.

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,22 +1,34 @@
 'use client';
 
 import { signIn } from 'next-auth/react';
-import { useForm } from 'react-hook-form';
+import { useForm, useFieldArray } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import { Card, Col, Container, Button, Form, Row } from 'react-bootstrap';
 import { createUser } from '@/lib/dbActions';
 
 type SignUpForm = {
+  firstName: string;
+  lastName: string;
+  userName: string;
   email: string;
   password: string;
   confirmPassword: string;
+  musicalGoals?: string;
+  instruments: { name: string }[];
+  experienceLevel: string[];
+  // selectMusicalTastes: string[];
   // acceptTerms: boolean;
 };
 
 /** The sign up page. */
 const SignUp = () => {
   const validationSchema = Yup.object().shape({
+    firstName: Yup.string().required('First Name is required'),
+    lastName: Yup.string().required('Last Name is required'),
+    userName: Yup.string()
+      .required('Username is required')
+      .max(20, 'Username must not exceed 20 characters'),
     email: Yup.string().required('Email is required').email('Email is invalid'),
     password: Yup.string()
       .required('Password is required')
@@ -25,15 +37,26 @@ const SignUp = () => {
     confirmPassword: Yup.string()
       .required('Confirm Password is required')
       .oneOf([Yup.ref('password'), ''], 'Confirm Password does not match'),
+    musicalGoals: Yup.string(),
   });
 
   const {
     register,
     handleSubmit,
     reset,
+    control,
     formState: { errors },
   } = useForm<SignUpForm>({
     resolver: yupResolver(validationSchema),
+    defaultValues: {
+      instruments: [],
+      experienceLevel: [],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray<SignUpForm, 'instruments'>({
+    control,
+    name: 'instruments',
   });
 
   const onSubmit = async (data: SignUpForm) => {
@@ -52,6 +75,45 @@ const SignUp = () => {
             <Card>
               <Card.Body>
                 <Form onSubmit={handleSubmit(onSubmit)}>
+                  <Row>
+                    <Col>
+                      <Form.Group className="form-group">
+                        <Form.Label>First Name</Form.Label>
+                        <input
+                          type="text"
+                          {...register('firstName')}
+                          className={`form-control ${errors.firstName ? 'is-invalid' : ''}`}
+                        />
+                        <div className="invalid-feedback">
+                          {errors.firstName?.message}
+                        </div>
+                      </Form.Group>
+                    </Col>
+                    <Col>
+                      <Form.Group className="form-group">
+                        <Form.Label>Last Name</Form.Label>
+                        <input
+                          type="text"
+                          {...register('lastName')}
+                          className={`form-control ${errors.lastName ? 'is-invalid' : ''}`}
+                        />
+                        <div className="invalid-feedback">
+                          {errors.lastName?.message}
+                        </div>
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                  <Form.Group className="form-group">
+                    <Form.Label>Username</Form.Label>
+                    <input
+                      type="text"
+                      {...register('userName')}
+                      className={`form-control ${errors.userName ? 'is-invalid' : ''}`}
+                    />
+                    <div className="invalid-feedback">
+                      {errors.userName?.message}
+                    </div>
+                  </Form.Group>
                   <Form.Group className="form-group">
                     <Form.Label>Email</Form.Label>
                     <input
@@ -85,6 +147,75 @@ const SignUp = () => {
                     <div className="invalid-feedback">
                       {errors.confirmPassword?.message}
                     </div>
+                  </Form.Group>
+                  <Form.Group className="form-group">
+                    <Form.Label>Musical Goals</Form.Label>
+                    <textarea
+                      {...register('musicalGoals')}
+                      className="form-control"
+                      rows={3}
+                    />
+                  </Form.Group>
+                  <Form.Group className="form-group py-3">
+                    <Row>
+                      <Col>
+                        {fields.map((field, index) => (
+                          <Row key={field.id} className="mb-3">
+                            <Col>
+                              <Form.Select
+                                {...register(`instruments.${index}.name`)}
+                              >
+                                <option value="">Select Instrument</option>
+                                <option value="guitar">Guitar</option>
+                                <option value="bass">Bass</option>
+                                <option value="drums">Drums</option>
+                                <option value="keyboard">Keyboard</option>
+                                <option value="vocals">Vocals</option>
+                                <option value="producer">Producer</option>
+                                <option value="other">Other</option>
+                              </Form.Select>
+                            </Col>
+                            <Col>
+                              <Form.Select
+                                {...register(`experienceLevel.${index}`)}
+                              >
+                                <option value="">Experience Level</option>
+                                <option value="beginner">Beginner</option>
+                                <option value="intermediate">
+                                  Intermediate
+                                </option>
+                                <option value="advanced">Advanced</option>
+                              </Form.Select>
+                            </Col>
+                            <Col xs="auto">
+                              <Button
+                                variant="danger"
+                                onClick={() => remove(index)}
+                              >
+                                Remove
+                              </Button>
+                            </Col>
+                          </Row>
+                        ))}
+                        <Button
+                          type="button"
+                          onClick={() => append({ name: '' })}
+                          className="btn btn-primary"
+                        >
+                          Add Instrument
+                        </Button>
+                      </Col>
+                    </Row>
+                  </Form.Group>
+                  <Form.Group className="form-group">
+                    <Button
+                      variant="primary"
+                      size="lg"
+                      className="d-grid gap-2"
+                      href=""
+                    >
+                      Select Musical Tastes
+                    </Button>
                   </Form.Group>
                   <Form.Group className="form-group py-3">
                     <Row>
