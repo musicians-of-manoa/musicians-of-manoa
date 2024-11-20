5edae75d181f10439a07eb2c6d509f558a7fa35b
splimon <splimon@hawaii.edu>
2024-11-20 11:39:48 -1000

    Update Searchbar.tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,16 +5,17 @@ import { Container, Col, Form } from 'react-bootstrap';
 import { Search } from 'react-bootstrap-icons';
 
 const EditJamInfoForm: React.FC = () => (
-  <Container>
+  <Container style={{ backgroundColor: '#ECDFCC' }} className="rounded">
     <Col className="text-center py-4" style={{ fontFamily: 'Arial' }}>
       <Form>
         <Form.Group className="d-flex">
           <Form.Control
             type="text"
+            className="rounded"
             placeholder="Search..."
             style={{ width: '1200px', height: '50px', marginRight: '10px' }}
           />
-          <Search style={{ paddingTop: '10px' }} />
+          <Search size="40px" style={{ paddingTop: '10px' }} />
         </Form.Group>
       </Form>
     </Col>
