0eef4900ba586bd54223e9c036c44f72acd3e3e6
Dahyun Kwon <142938702+dhk486@users.noreply.github.com>
2024-11-21 16:19:49 -1000

    Changed Navbar.Toggle Color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,16 @@
 import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import { Container, Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
-import { BoxArrowRight, Lock, Person, PersonFill,
-         PersonPlusFill, Search, MusicNoteBeamed,
-         CardList } from 'react-bootstrap-icons';
+import {
+  BoxArrowRight,
+  Lock,
+  Person,
+  PersonFill,
+  PersonPlusFill,
+  Search,
+  MusicNoteBeamed,
+  CardList,
+} from 'react-bootstrap-icons';
 
 const NavBar: React.FC = () => {
   const { data: session } = useSession();
@@ -26,37 +33,50 @@ const NavBar: React.FC = () => {
             height={80}
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="custom-toggler"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-start">
-          {currentUser
-            ? [
-                <Nav.Link
-                  id="feed-nav"
-                  href="/feed"
-                  key="feed"
-                  active={pathName === '/feed'}
-                >
-                  Feed
-                  <CardList className="ms-2" />
-                </Nav.Link>,
-                <Nav.Link
-                  id="create-jam-nav"
-                  href="/jam-information"
-                  key="create-jam"
-                  active={pathName === '/jam-information'}
-                >
-                  Create a Jam
-                  <MusicNoteBeamed className="ms-2" />
-                </Nav.Link>,
-                 <Nav.Link id="search-nav" href="/search" key="search" active={pathName === '/search'}>
-                 Search
-                 <Search className="ms-2" />
-                 </Nav.Link>,
-              ]
-            : ''}
+            {currentUser
+              ? [
+                  <Nav.Link
+                    id="feed-nav"
+                    href="/feed"
+                    key="feed"
+                    active={pathName === '/feed'}
+                  >
+                    Feed
+                    <CardList className="ms-2" />
+                  </Nav.Link>,
+                  <Nav.Link
+                    id="create-jam-nav"
+                    href="/jam-information"
+                    key="create-jam"
+                    active={pathName === '/jam-information'}
+                  >
+                    Create a Jam
+                    <MusicNoteBeamed className="ms-2" />
+                  </Nav.Link>,
+                  <Nav.Link
+                    id="search-nav"
+                    href="/search"
+                    key="search"
+                    active={pathName === '/search'}
+                  >
+                    Search
+                    <Search className="ms-2" />
+                  </Nav.Link>,
+                ]
+              : ''}
             {currentUser && role === 'ADMIN' ? (
-              <Nav.Link id="admin-stuff-nav" href="/admin" key="admin" active={pathName === '/admin'}>
+              <Nav.Link
+                id="admin-stuff-nav"
+                href="/admin"
+                key="admin"
+                active={pathName === '/admin'}
+              >
                 Admin
               </Nav.Link>
             ) : (
@@ -66,26 +86,41 @@ const NavBar: React.FC = () => {
           <Nav>
             {session ? (
               <NavDropdown id="login-dropdown" title={currentUser}>
-                <NavDropdown.Item id="login-dropdown-view-profile" href="/profile">
-                 <Person />
+                <NavDropdown.Item
+                  id="login-dropdown-view-profile"
+                  href="/profile"
+                >
+                  <Person />
                   View/Edit Profile
                 </NavDropdown.Item>
-                <NavDropdown.Item id="login-dropdown-sign-out" href="/api/auth/signout">
+                <NavDropdown.Item
+                  id="login-dropdown-sign-out"
+                  href="/api/auth/signout"
+                >
                   <BoxArrowRight />
                   Sign Out
                 </NavDropdown.Item>
-                <NavDropdown.Item id="login-dropdown-change-password" href="/auth/change-password">
+                <NavDropdown.Item
+                  id="login-dropdown-change-password"
+                  href="/auth/change-password"
+                >
                   <Lock />
                   Change Password
                 </NavDropdown.Item>
               </NavDropdown>
             ) : (
               <NavDropdown id="login-dropdown" title="Login">
-                <NavDropdown.Item id="login-dropdown-sign-in" href="/auth/signin">
+                <NavDropdown.Item
+                  id="login-dropdown-sign-in"
+                  href="/auth/signin"
+                >
                   <PersonFill />
                   Sign in
                 </NavDropdown.Item>
-                <NavDropdown.Item id="login-dropdown-sign-up" href="/auth/signup">
+                <NavDropdown.Item
+                  id="login-dropdown-sign-up"
+                  href="/auth/signup"
+                >
                   <PersonPlusFill />
                   Sign up
                 </NavDropdown.Item>
