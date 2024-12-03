ff114ca7abee2299a5c795df5837956b020345b7
ChristianI-HI <ihac.school@gmail.com>
2024-12-02 23:09:23 -1000

    Playwright Availability Tests Integration

--- tests/admin-pages.spec.ts
+++ tests/admin-pages.spec.ts
@@ -5,17 +5,31 @@ test.use({
 });
 
 test('Admin Pages', async ({ page }) => {
+  // Home Page
   await page.goto('http://localhost:3000/');
-  await expect(page.getByRole('link', { name: 'Next.js Application Template' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Add Stuff' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'List Stuff' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Musicians of Manoa Logo' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Feed' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Create a Jam' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Search' })).toBeVisible();
   await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
   await expect(page.getByRole('button', { name: 'admin@foo.com' })).toBeVisible();
-  await page.getByRole('link', { name: 'Add Stuff' }).click();
-  await expect(page.getByRole('heading', { name: 'Add Stuff' })).toBeVisible();
-  await page.getByRole('link', { name: 'List Stuff' }).click();
-  await expect(page.getByRole('heading', { name: 'Stuff' })).toBeVisible();
+
+  // Check user pages
+  await page.getByRole('link', { name: 'Feed' }).click();
+  await expect(page.getByRole('heading', { name: 'Upcoming Jams' })).toBeVisible();
+  await page.getByRole('link', { name: 'Create a Jam' }).click();
+  await expect(page.getByRole('heading', { name: 'Jam Information' })).toBeVisible();
+  await page.getByRole('link', { name: 'Search' }).click();
+  await expect(page.getByRole('heading', { name: 'Search for...' })).toBeVisible();
+
+  // Check admin-only pages
   await page.getByRole('link', { name: 'Admin' }).click();
-  await expect(page.getByRole('heading', { name: 'List Stuff Admin' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'List Users Admin' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Admin Dashboard' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'List of Users' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Edit Jams (Admin)' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Edit Experience Levels' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Edit Musical Goals' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Edit Musical Tastes' })).toBeVisible();
+  await page.getByRole('button', { name: 'Edit Musical Goals' }).click();
+  await expect(page.getByRole('heading', { name: 'Editing Musical Goals' })).toBeVisible();
 });
